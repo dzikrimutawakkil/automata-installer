41bce65dfdb3570266a141237a47d2b208ce193d
dzikriMA <dzikri.mutawakkil@bee.id>
2025-05-27 08:55:54 +0700

    make a builder

---
+++ build_installer.bat
@@ -0,0 +1,15 @@
+@echo off
+echo [*] Cleaning previous build...
+rmdir /s /q build
+rmdir /s /q dist
+del /q installer.spec
+
+echo [*] Building installer.exe...
+pyinstaller --onefile ^
+  --add-data "commandlinetools.zip;." ^
+  --add-data "java-17.zip;." ^
+  --add-data "AutoMata-Test Tools.zip;." ^
+  installer.py
+
+echo [*] Done. Find installer.exe in the /dist folder.
+pause
